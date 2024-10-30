2d1812727f7d2738d3992490ea55d87d632332b7
Wiktor <wiktor.bachta@gmail.com>
2024-10-30 18:52:31 +0100

    aod lab1 finished

--- sem 5/AOD/lab1/make_source_graph.py
+++ sem 5/AOD/lab1/make_source_graph.py
@@ -1,28 +1,44 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
+graphs = ["1/g4d.txt", "1/g4u.txt", "2/g2a-moj.txt", "2/g2b-moj.txt",
+          "3/g3-moj1.txt", "3/g3-moj2.txt", "4/d4a-moj.txt", "4/d4b-moj.txt", "4/u4a-moj.txt", "4/u4b-moj.txt"]
 
-with open("./aod_testy1/4/d4a-moj.txt", "r") as input_file:
-    directed = False
-    if input_file.readline().strip() == "D":
-        directed = True
+for graph in graphs:
+    with open("./aod_testy1/" + graph, "r") as input_file:
+        directed = False
+        if input_file.readline().strip() == "D":
+            directed = True
 
-    vertex_num = int(input_file.readline())
-    edges = []
+        vertex_num = int(input_file.readline())
+        edges = []
 
-    edge = input_file.readline()  # skip edge count
-    edge = input_file.readline()
-    while edge:
-        edges.append((int(edge.split(" ")[0]), int(edge.split(" ")[1])))
+        edge = input_file.readline()  # skip edge count
         edge = input_file.readline()
+        while edge:
+            edges.append((int(edge.split(" ")[0]), int(edge.split(" ")[1])))
+            edge = input_file.readline()
 
-if directed:
-    G = nx.MultiDiGraph()
-    G.add_edges_from(edges)
-    nx.draw(G, with_labels=True, pos=nx.planar_layout(G))
-    plt.show()
-else:
-    G = nx.MultiGraph()
-    G.add_edges_from(edges)
-    nx.draw(G, with_labels=True, pos=nx.planar_layout(G))
-    plt.show()
+    if directed:
+        G = nx.MultiDiGraph()
+        G.add_edges_from(edges)
+        plt.figure(figsize=(10, 8), dpi=150)
+        try:
+            nx.draw(G, with_labels=True, pos=nx.planar_layout(G))
+        except nx.exception.NetworkXException:
+            nx.draw(G, with_labels=True, pos=nx.kamada_kawai_layout(G))
+        plt.savefig("./images/" + graph[2:-4] + ".jpg", bbox_inches='tight')
+        plt.figure()
+        # plt.show()
+    else:
+        G = nx.MultiGraph()
+        G.add_edges_from(edges)
+        plt.figure(figsize=(10, 8), dpi=150)
+
+        try:
+            nx.draw(G, with_labels=True, pos=nx.planar_layout(G))
+        except nx.exception.NetworkXException:
+            nx.draw(G, with_labels=True, pos=nx.kamada_kawai_layout(G))
+        plt.savefig("./images/" + graph[2:-4] + ".jpg", bbox_inches='tight')
+        plt.figure()
+        # plt.show()
--- sem 5/AOD/lab1/src/timing.cpp
+++ sem 5/AOD/lab1/src/timing.cpp
@@ -7,32 +7,66 @@
 
 int main(void)
 {
-    std::ofstream out_file("./zad1.txt");
-    out_file << "n,m,td,tb\n";
+    std::ofstream out_file("./results.txt");
+    out_file << "nazwa, skierowany, |V| + |E|, czas\n";
     out_file.close();
 
-    std::vector<std::string> filenames = {"g1d.txt", "g1u.txt", "g2d.txt", "g2u.txt", "g3d.txt", "g3u.txt", "g4d.txt", "g4u.txt"};
+    std::vector<std::string> filenames = {"g3-1.txt", "g3-2.txt", "g3-3.txt", "g3-4.txt", "g3-5.txt", "g3-6.txt"};
 
     for (const auto &filename : filenames)
     {
-        Graph g("./aod_testy1/1/" + filename);
+        Graph g("./aod_testy1/3/" + filename);
+        std::string directed;
+        if (g.is_directed)
+        {
+            directed = "skierowany";
+        }
+        else
+        {
+            directed = "nieskierowany";
+        }
         auto begin = std::chrono::high_resolution_clock::now();
         dfs(g, false, false);
-        auto mid = std::chrono::high_resolution_clock::now();
-        bfs(g, false, false);
         auto end = std::chrono::high_resolution_clock::now();
 
-        auto dfs_time = std::chrono::duration_cast<std::chrono::nanoseconds>(mid - begin).count();
-        auto bfs_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - mid).count();
+        auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
-        std::ofstream out_file;
-        out_file.open("./zad1.txt", std::ios_base::app);
-        out_file << g.vertex_num << "," << g.edges_num << "," << dfs_time << "," << bfs_time << "\n";
+        out_file.open("./results.txt", std::ios_base::app);
+        out_file << filename << "," << directed << "," << g.edges_num + g.vertex_num << "," << time << "\n";
         out_file.close();
     }
 
-    out_file = std::ofstream("./zad2.txt");
-    out_file << "n,m,t\n";
+    out_file.open("./results.txt", std::ios_base::app);
+    out_file << "nazwa, skierowany, |V| + |E|, czas\n";
+    out_file.close();
+
+    filenames = {"g3-1.txt", "g3-2.txt", "g3-3.txt", "g3-4.txt", "g3-5.txt", "g3-6.txt"};
+
+    for (const auto &filename : filenames)
+    {
+        Graph g("./aod_testy1/3/" + filename);
+        std::string directed;
+        if (g.is_directed)
+        {
+            directed = "skierowany";
+        }
+        else
+        {
+            directed = "nieskierowany";
+        }
+        auto begin = std::chrono::high_resolution_clock::now();
+        bfs(g, false, false);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
+        out_file.open("./results.txt", std::ios_base::app);
+        out_file << filename << "," << directed << "," << g.edges_num + g.vertex_num << "," << time << "\n";
+        out_file.close();
+    }
+
+    out_file.open("./results.txt", std::ios_base::app);
+    out_file << "nazwa, skierowany, |V| + |E|, czas\n";
     out_file.close();
 
     filenames = {"g2a-1.txt", "g2b-1.txt", "g2a-2.txt", "g2b-2.txt", "g2a-3.txt", "g2b-3.txt",
@@ -41,41 +75,57 @@ int main(void)
     for (const auto &filename : filenames)
     {
         Graph g("./aod_testy1/2/" + filename);
+        std::string directed;
+        if (g.is_directed)
+        {
+            directed = "skierowany";
+        }
+        else
+        {
+            directed = "nieskierowany";
+        }
         auto begin = std::chrono::high_resolution_clock::now();
         is_acyclic(g, false);
         auto end = std::chrono::high_resolution_clock::now();
 
         auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
-        std::ofstream out_file;
-        out_file.open("./zad2.txt", std::ios_base::app);
-        out_file << g.vertex_num << "," << g.edges_num << "," << time << "\n";
+        out_file.open("./results.txt", std::ios_base::app);
+        out_file << filename << "," << directed << "," << g.edges_num + g.vertex_num << "," << time << "\n";
         out_file.close();
     }
 
-    out_file = std::ofstream("./zad3.txt");
-    out_file << "n,m,t\n";
+    out_file.open("./results.txt", std::ios_base::app);
+    out_file << "nazwa, skierowany, |V| + |E|, czas\n";
     out_file.close();
 
-    filenames = {"g3-1.txt", "g3-2.txt", "g3-3.txt", "g3-4.txt", "g3-5.txt", "g3-6.txt"};
+    filenames = {"g3-1.txt", "g3-2.txt", "g3-3.txt", "g3-4.txt", "g3-5.txt"};//, "g3-6.txt"};
 
     for (const auto &filename : filenames)
     {
         Graph g("./aod_testy1/3/" + filename);
+        std::string directed;
+        if (g.is_directed)
+        {
+            directed = "skierowany";
+        }
+        else
+        {
+            directed = "nieskierowany";
+        }
         auto begin = std::chrono::high_resolution_clock::now();
         get_connected_components(g);
         auto end = std::chrono::high_resolution_clock::now();
 
         auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
-        std::ofstream out_file;
-        out_file.open("./zad3.txt", std::ios_base::app);
-        out_file << g.vertex_num << "," << g.edges_num << "," << time << "\n";
+        out_file.open("./results.txt", std::ios_base::app);
+        out_file << filename << "," << directed << "," << g.edges_num + g.vertex_num << "," << time << "\n";
         out_file.close();
     }
 
-    out_file = std::ofstream("./zad4.txt");
-    out_file << "n,m,t\n";
+    out_file.open("./results.txt", std::ios_base::app);
+    out_file << "nazwa, skierowany, |V| + |E|, czas\n";
     out_file.close();
 
     filenames = {"d4a-1.txt", "d4b-1.txt", "u4a-1.txt", "u4b-1.txt",
@@ -88,15 +138,23 @@ int main(void)
     for (const auto &filename : filenames)
     {
         Graph g("./aod_testy1/4/" + filename);
+        std::string directed;
+        if (g.is_directed)
+        {
+            directed = "skierowany";
+        }
+        else
+        {
+            directed = "nieskierowany";
+        }
         auto begin = std::chrono::high_resolution_clock::now();
         is_bapartite(g, false);
         auto end = std::chrono::high_resolution_clock::now();
 
         auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
-        std::ofstream out_file;
-        out_file.open("./zad4.txt", std::ios_base::app);
-        out_file << g.vertex_num << "," << g.edges_num << "," << time << "\n";
+        out_file.open("./results.txt", std::ios_base::app);
+        out_file << filename << "," << directed << "," << g.edges_num + g.vertex_num << "," << time << "\n";
         out_file.close();
     }
 }
